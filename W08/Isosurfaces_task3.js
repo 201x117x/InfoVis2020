b9e13f11d5022ab57caf52f70e45e52b73fad476
201x117x <201x117x@stu.kobe-u.ac.jp>
2020-06-09 00:25:22 +0900

    add W08 task2,3

--- W08/Isosurfaces_task3.js
+++ W08/Isosurfaces_task3.js
@@ -1,7 +1,15 @@
-function Isosurfaces( volume, isovalue )
+function Isosurfaces( volume, isovalue, screen )
 {
     var geometry = new THREE.Geometry();
-    var material = new THREE.MeshLambertMaterial();
+    // var material = new THREE.MeshLambertMaterial();
+    var material = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('phong.vert').text,
+        fragmentShader: document.getElementById('phong.frag').text,
+        uniforms: {
+            light_position: { type: 'v3', value: screen.light.position }
+        }
+    });
 
     var smin = volume.min_value;
     var smax = volume.max_value;
